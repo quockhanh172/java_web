3b9ad310c8b4e273570dd964c6492a0f8bb06ec6
quockhanh1702 <74188095+quockhanh1702@users.noreply.github.com>
2022-05-23 08:54:57 +0700

    user detail and userdetailservice

---
+++ demo/src/main/java/com/java/auth/ApplicationUser.java
@@ -0,0 +1,63 @@
+package com.java.auth;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ApplicationUser implements UserDetails{
+
+	private final List<? extends GrantedAuthority> grantedAuthorities;
+	private final String password;
+	private final String username;
+	private final boolean isAccountNonExpired;
+	private final boolean isAccountNonLocked;
+	private final boolean isCredentialsNonExpired;
+	private final boolean isEnabled;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
